5d5167dadd1055f371c66cada505923b8146757f
James Santos-Calacat <jamessantosc@gmail.com>
2025-03-01 01:06:00 -0800

    Fix main.tf formatting

--- main.tf
+++ main.tf
@@ -13,32 +13,32 @@ output "fastly_services_version" {
 }
 
 resource "fastly_service_vcl" "service" {
-  name    = var.service_vcl.name
-  comment = var.service_vcl.comment
+  name           = var.service_vcl.name
+  comment        = var.service_vcl.comment
   force_destroy  = var.service_vcl.force_destroy
   stale_if_error = var.service_vcl.stale_if_error
 
   domain {
-    name = var.domain.name
+    name    = var.domain.name
     comment = var.domain.comment
   }
 
-  dynamic backend {
+  dynamic "backend" {
     for_each = var.backends
 
     content {
-      name = backend.value["name"]
-      address = backend.value["address"]
-      ssl_cert_hostname = backend.value["ssl_cert_hostname"]
-      ssl_sni_hostname = backend.value["ssl_sni_hostname"]
-      weight = backend.value["weight"]
-      use_ssl = backend.value["use_ssl"]
-      max_conn = backend.value["max_conn"]
-      connect_timeout = backend.value["connect_timeout"]
-      first_byte_timeout = backend.value["first_byte_timeout"]
+      name                  = backend.value["name"]
+      address               = backend.value["address"]
+      ssl_cert_hostname     = backend.value["ssl_cert_hostname"]
+      ssl_sni_hostname      = backend.value["ssl_sni_hostname"]
+      weight                = backend.value["weight"]
+      use_ssl               = backend.value["use_ssl"]
+      max_conn              = backend.value["max_conn"]
+      connect_timeout       = backend.value["connect_timeout"]
+      first_byte_timeout    = backend.value["first_byte_timeout"]
       between_bytes_timeout = backend.value["between_bytes_timeout"]
-      override_host = backend.value["override_host"]
-      port = backend.value["port"]
+      override_host         = backend.value["override_host"]
+      port                  = backend.value["port"]
     }
   }
 
@@ -64,9 +64,9 @@ resource "fastly_service_vcl" "service" {
   }
 
   gzip {
-    name = "custom-compression-policy"
-    content_types = var.gzip_content_types
-    extensions = var.gzip_extensions
+    name            = "custom-compression-policy"
+    content_types   = var.gzip_content_types
+    extensions      = var.gzip_extensions
     cache_condition = "status_200_or_404"
   }
 
