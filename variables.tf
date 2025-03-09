c5ef4f7610661143909649d8713a5e5a035dfcdb
James Santos-Calacat <jamessantosc@gmail.com>
2025-03-09 09:46:50 -0700

    Add default values to gzip_extensions and gzip_content_types

--- variables.tf
+++ variables.tf
@@ -5,10 +5,41 @@ variable "env" {
 
 variable "gzip_extensions" {
   type = list(string)
+  default = [
+    "css",
+    "js",
+    "html",
+    "eot",
+    "ico",
+    "otf",
+    "ttf",
+    "json",
+    "svg"
+  ]
 }
 
 variable "gzip_content_types" {
   type = list(string)
+  default = [
+    "text/html",
+    "application/x-javascript",
+    "text/css",
+    "application/javascript",
+    "text/javascript",
+    "application/json",
+    "application/vnd.ms-fontobject",
+    "application/x-font-opentype",
+    "application/x-font-truetype",
+    "application/x-font-ttf",
+    "application/xml",
+    "font/eot",
+    "font/opentype",
+    "font/otf",
+    "image/svg+xml",
+    "image/vnd.microsoft.icon",
+    "text/plain",
+    "text/xml"
+  ]
 }
 
 variable "domains" {
@@ -73,4 +104,3 @@ variable "service_vcl" {
 #     common_name           = string
 #   })
 # }
-
